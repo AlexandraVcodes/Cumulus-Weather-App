d623681e77d92e4d6b9e279fd39d4606a7836772
Alexandra_V <alexandra.vukits82@gmail.com>
2024-05-08 12:57:08 +0200

    Added the icon to js via API, changed some css styling

--- src/javascript.js
+++ src/javascript.js
@@ -7,6 +7,9 @@ function refreshWeather(response) {
   let windVariable = document.querySelector("#wind");
   let timeVariable = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconVariable = document.querySelector("#icon");
+
+  iconVariable.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
   cityVariable.innerHTML = response.data.city;
   descriptionVariable.innerHTML = response.data.condition.description;
