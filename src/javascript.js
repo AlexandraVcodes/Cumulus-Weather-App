689f6aedc13e9f4fa77136fb9bbfb674f514fa00
Alexandra_V <alexandra.vukits82@gmail.com>
2024-05-19 22:02:08 +0200

    updated javascript

--- src/javascript.js
+++ src/javascript.js
@@ -77,17 +77,20 @@ function displayForecast(response) {
       forecastHtml =
         forecastHtml +
         `
-<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${formatDay(day.time)}</div>
-            <div class="weather-forecast-icon"> <img src="${
-              day.condition.icon_url
-            }" /></div>
-            <div class="weather-forecast-temperatures">
-              <div class="weather-forecast-temperature-max"> 
-              ${Math.round(day.temperature.maximum)}°</div>
-              <div class="weather-forecast-temperature-min">
-              ${Math.round(day.temperature.minimum)}° </div>
-`;
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+          </div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
+        </div>
+      </div>
+    `;
     }
   });
 
